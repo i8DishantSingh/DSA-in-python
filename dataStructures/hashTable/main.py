6062033dd385c830542efc9051e32d72f89c096e
i8DishantSingh <professional.id.dishant@gmail.com>
2026-04-30 13:13:49 +0530

    Implemented HashTable DataStructure

---
+++ dataStructures/hashTable/main.py
@@ -0,0 +1,33 @@
+class HashTable:
+    def __init__(self) -> None:
+        self.collection = {}
+
+    def hash(self, key: str) -> int:
+        unicode_sum = 0
+        for i in list(key.strip()):
+            ind_unicode = ord(i)
+            unicode_sum += ind_unicode
+        return unicode_sum
+    
+    def add(self, key, value):
+        key_hash = self.hash(key.strip())
+        if key_hash not in self.collection:
+            self.collection[key_hash] = {}
+        self.collection[key_hash][key] = value
+
+        
+
+    def remove(self, key):
+        key_hash = self.hash(key)
+        if key_hash in self.collection:
+            if key in self.collection[key_hash]:
+                def self.collection[key_hash][key]:
+                if not self.collection[key_hash]:
+                    del self.collection[key_hash]
+        
+
+    def lookup(self, key):
+        key_hash = self.hash(key)
+        if key_hash in self.collection:
+            return self.collection[key_hash].get(key, None)
+        return None
